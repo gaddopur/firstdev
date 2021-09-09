52eb67987f86ca8284a34979f955abf74fe5b4dc
Aman <amansingh.eee18@itbhu.ac.in>
2021-09-09 21:43:41 +0530

    removed testapp

--- firstdev/settings.py
+++ firstdev/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'mptt',
     'tinymce',
-    'testapp',
 ]
 
 MIDDLEWARE = [
--- testapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TestApp(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField()
-    text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
