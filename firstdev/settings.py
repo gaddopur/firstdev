1cc19f3f7cfdf427e32b4fc47760c56d58b34eea
Aman Singh <amasingh@confluent.io>
2024-07-08 23:03:03 +0530

    run in debug mode

--- firstdev/settings.py
+++ firstdev/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['gaddopurblog.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', "*"]
 
